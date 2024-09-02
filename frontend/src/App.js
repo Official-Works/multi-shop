792312e8a369f859c9b172cde6cda3fe4d42f27f
official-works <official0works@gmail.com>
2024-09-02 09:18:26 +0530

    Ready to deploy backend on render

--- frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Layout from "./layout/layout";
-import './App.css'
-
-function App() {
-  return <Layout />;
-}
-
-export default App;
--- frontend/src/config.js
+++
@@ -1,5 +0,0 @@
-/** @format */
-
-export const BASE_URL = "http://localhost:5000/api";
-export const AUTH_URL = "http://localhost:5000";
-export const token = localStorage.getItem("token");
